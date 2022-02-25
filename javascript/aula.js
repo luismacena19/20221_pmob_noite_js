231f74ad48e8b8423e1eec990b74a1113f27620b
‘Luis <luis.macena@outlook.com>
2022-02-25 18:31:58 -0300

    Retomar da secao 1.5 da apostila 02 falando de vetores

--- javascript/aula.js
+++ javascript/aula.js
@@ -1,1 +1,59 @@
-console.log("aa");
+//console.log("aa");
+//
+//const cosnti = "constante";
+//var bi = "n use, n possui escopo"
+//let ba = "use, tem escopo"
+//
+//const nome = "Jose"
+//const idade = 27
+//nome = "Maria"
+//const a2
+//
+//let a = 2
+//let b = 'abc'
+//let endereco = `Rua K, 12`
+//
+//var linguagem = "Javascript"
+//console.log("Aprendendo, " + linguagem)
+//var linguagem = "Java"
+//console.log("Aprendendo, " + linguagem)
+
+//var idade = 18
+//console.log("Oi, " + nome)
+//if(idade >= 18){
+//    // içamento ou hoist Quando var nome vai pra fora do bloco
+//    var nome = "João"
+//    //console.log("Parabens, " + nome + ". Voce já pode dirigir.")
+//    console.log(`Parabens, ${nome}. Voce já pode dirigir.`)
+//}
+//console.log("Até mais, " + nome + ".")
+
+// //coerção implicita
+// const n1 = 2
+// const n2 ='3'
+// const n3 = n1 + n2
+// console.log(n3)
+// //coreção explicita
+// const n4 = n1 + Number(n2)
+// //const n4 = n1 + +n2
+// console.log(n4)
+
+//operador de igualdade ==(Faz coerção) e ===(Não faz coerção, ele primeiro faz a comparação de tipo)
+// console.log(1 == 1)
+// console.log(1 == '1')
+// console.log(1 === '1')
+// console.log(true == 1)
+// console.log(1 == [1])
+// console.log(null == null)
+// console.log(null == undefined)
+// console.log([] == false)
+// console.log([] == [])
+// const a = []
+// const b = a
+// console.log(a == b)
+
+const nome = ""
+console.log(!!nome)
+console.log(nome !== "")
+const nome2 = "Lulu"
+console.log(!!nome2)
